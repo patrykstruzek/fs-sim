e03e818c4a5a2ae6828809292368764f17fbe814
patrykstruzek <patrykstruzek17@gmail.com>
2023-07-30 22:07:55 +0200

    Add first test case

---
+++ src/tests/test.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests {
+    use fs_lib::directory::*;
+    use fs_lib::file::*;
+
+    #[test]
+    fn dir_should_store_file() {
+        let mut new_dir = Directory::new("dir");
+        let new_file = File::new("file", "txt", 0);
+
+        new_dir.store(new_file.clone());
+
+        assert_eq!(new_dir.files().get(0).unwrap(), &new_file);
+    }
+}
