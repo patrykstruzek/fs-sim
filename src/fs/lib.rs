2b2a69c8002b1d9d53089832672fd0c31c0db681
patrykstruzek <patrykstruzek17@gmail.com>
2023-08-19 23:36:45 +0200

    Add util functions and delete previous utils module

--- src/fs/lib.rs
+++ src/fs/lib.rs
@@ -3,5 +3,19 @@ pub mod dir;
 pub mod fl;
 pub mod fs;
 pub mod usr;
-pub mod utils;
-pub use utils::*;
+pub mod db;
+
+// utils:
+
+use std::io::*;
+
+pub fn clr_curr_ln() {
+    print!("\x1B[2K");
+    print!("\x1B[1F");
+    std::io::stdout().flush().unwrap();
+}
+
+pub fn print(text: &str) {
+    print!("{}", text);
+    std::io::stdout().flush().unwrap();
+}
