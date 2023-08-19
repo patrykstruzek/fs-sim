db6d01d86ff79e97def27620e1585dbc6eb2a1ef
patrykstruzek <patrykstruzek17@gmail.com>
2023-08-19 23:37:08 +0200

    Small fix for test

--- src/tests/test.rs
+++ src/tests/test.rs
@@ -2,6 +2,8 @@
 mod tests {
     use fs_lib::dir::*;
     use fs_lib::fl::*;
+    use std::cell::RefMut;
+    use std::rc::Rc;
 
     #[test]
     fn dir_should_store_file() {
@@ -10,6 +12,9 @@ mod tests {
 
         new_dir.store(new_file.clone());
 
-        assert_eq!(new_dir.files().borrow().get(0), new_file);
+        let file_ref: Rc<File> = Rc::new(new_file);
+        let stored_files: RefMut<Vec<Rc<File>>> = new_dir.files().borrow_mut();
+
+        assert_eq!(stored_files.get(0), Some(&file_ref));
     }
 }
